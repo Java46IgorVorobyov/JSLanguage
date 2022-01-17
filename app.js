2cc3d374663d71ac93b36294341e400be5c66095
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-01-17 21:05:57 +0200

    classwork 12

--- app.js
+++ app.js
@@ -1,27 +1,80 @@
-function encode(num, codingString){
-    let res = "";
-    if(checkDuplicate(codingString) == true){
-        console.log('Error: checkDuplicate symbols in coding string');
+const ar = [2, 100, 9, 80];
+// ar.sort(function(a, b){
+//     return a - b;
+// });
+// ar.sort((a, b) => a - b);
+// console.log(`ar is ${ar.toString()}`);
 
+
+
+//adding element/s at and of an array 
+
+ar[ar.length] = 200;
+console.log(ar);
+ar.push(300, 25);
+console.log(ar);
+const ar1 = [-8, 30, -57];
+// ar.push(ar1); //there will be added array but not separated number 
+// if you want to add separated numbers you should apply a spread operator ...
+ar.push(...ar1);
+console.log(ar);
+// for adding elements at beginning of an array there is method "unshift"
+// everything has been said about "push" will work for "unshift" omly unlike "push" unshift method 
+// adds elements at beginning
+
+const ar2 = [27, 35];
+ar.unshift(...ar2);
+console.log(ar);
+
+// method splice for inserting or replacing any elements in any place
+// first argument - index for inserting or replacing 
+// second argument - number of the deleted elements (if 0 than splice will be applied only for inserting)
+// third argument designates sequence of being inserted elements like push/unshift
+
+ar.splice(2, 0, 1, 2);
+console.log(ar);
+
+// removing (deleting)
+
+let el = ar.pop(); //removes the last element and returns it 
+// el - -57; -57 will be remove from array ar
+el = ar.shift();//removes the first element and returns it 
+// el = 27; 27 will be removed from the array ar
+console.log(el, ar);
+
+ar.splice(3,5); //remove 5 elements begining from the one at index 3
+console.log(ar);
+
+// includes, indexOf, lastIndexOf
+
+// two dimensional arrays
+const matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+const matrix2 = [[100, 20], [50, -5], [34, 28]];
+function displayMatrix(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        let row = '';
+            for (let j = 0; j < matrix[i].length; j++){
+                row = row + matrix[i][j] + '  ';
+            }
+            console.log(row);
     }
-    else{
-    do{
-   const digit = Math.trunc(num % (codingString.length-1));
-   const symb = getSymbol(digit, codingString);
-   res = symb + res;
-   num = Math.trunc(num / (codingString.length-1));
-    } while(num >= 1);
-    return res;
 }
-function getSymbol(digit, codinString) {
+// displayMatrix(matrix1);
+displayMatrix(matrix2);
 
-    return codinString[digit];
+//HW12 definition
+//const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+//четные в порядке возрастания не четные в порядке убывания 
+//expected [-10, 4, 28, 40, 27, 25, 17, 15, 13];
+//task: write a comparator (a comporator returns < 0 if first less than second, > 0
+//if first greater than second, ==0 if first equals second)
+//--------
+//write function 
+
+function matrixTransp(matrix) {
+    //TODO
+    //returns matrix with columns that are rows of the source matrix and 
+    //rows that are columns of the source matrix
+    //example: source matrix[[1, 2], [3, 4], [5, 6]]
+    //result  matrix [[1, 3, 5], [2, 4, 6]]
 }
-
-}
-function checkDuplicate(codingString){
-
-    return new Set(codingString).size != codingString.length;
-}
-console.log(encode(22,'igor'));
-
