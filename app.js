002cd37f818be4c998d8c9b1d0b5349a76ca127c
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-01-18 11:24:54 +0200

    arHw version2

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
-arHw.sort((a, b) => (a % 2 - b % 2) || (a % 2 ? b - a : a - b));
-console.log(arHw);
+let odds = arHw.filter((a) => a % 2 !== 0).sort((a, b) => b - a);
+let even = arHw.filter((a) => a % 2 === 0).sort((a, b) => a - b);
+let sorted = even.concat(odds);
+console.log(sorted);
+
 
 
 const matrix1 = [
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,15 +1,7 @@
-const matrix1 = [[1, 2], [3, 4], [5, 6]];
-function matrixTransp(matrix) {
-    const rows = matrix.length, cols = matrix[0].length;
-  const grid = [];
-  for (let j = 0; j < cols; j++) {
-    grid[j] = Array(rows);
-  }
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      grid[j][i] = matrix[i][j];
-    }
-  }
-  console.log(grid);
-}
-matrixTransp(matrix1);
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+// arHw.sort((a, b) => (a % 2 - b % 2) || (a % 2 ? b - a : a - b));
+let odds = arHw.filter((a) => a % 2 !== 0).sort((a, b) => b - a);
+let even = arHw.filter((a) => a % 2 === 0).sort((a, b) => a - b);
+let sorted = even.concat(odds);
+console.log(sorted);
+//console.log(arHw);
