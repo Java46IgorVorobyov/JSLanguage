9d70dc10be04dd9f864414a5f944ecc7cc3bb9b0
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-01-21 09:01:56 +0200

    forEach, map array methods

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-const comparator = [13, 28, 4, 15, 25, -10, 40, 17, 27];
-comparator.sort((a, b) => (a % 2 - b % 2) || (a % 2 ? b - a : a - b));
-console.log(comparator);
+const ar = [-10, 50, -12, 80, 40];
+ar.push(70);
+// str - "-10#50#-12#80#40#70#"
+//*********solution based on substring */
+// let str = '';
+// ar.forEach(function(n){
+//     str += n + '#';
+// });
+// //str = str.substring(0, str.length-1);
+//********solution base on forEach from second number */
+// const ar1 = ar.slice(1);
+// let str = '' + ar[0];
+// ar1.forEach(n => str += '#' + n);
+// console.log(str);
 
+//***printing out sequense number of element, element, length of array */
+//1 of 5 - -10;
 
+ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
 
+//Home Work 13 task1 definition
+//write function myForEach(array, callback-function);
+//array - being iteration array
+//callback-function - function that will be called for each element of array.
+//callback-function should take three arguments: 
+//1. current element; 2. current index; 3. being iterated array
+//example of standart forEach: ar1.forEach(n => str += '#' + n);
+//example of my forEach: myForEach(array, n => str += '#' + n);
 
-const matrix1 = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-];
+//************************************* */
+//method "map"
+//use case of applying method map: you want to create new array with elements that ar received as result of some convertion
+//example you want to get new array with elements that are multiplication on 2 of each source element
+const ar2 = ar.map(n => n * 2);
+console.log(ar2);
+const ar3 = ar.map((n, i, a) => `<li>${i + 1} of ${a.length} - ${n}</li>`);
+console.log(ar3);
 
-function matrixTransp(matrix) {
-    const rows = matrix.length,
-          cols = matrix[0].length;
-    const grid = [];
-    for (let j = 0; j < cols; j++) {
-        grid[j] = Array(rows);
-    }
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            grid[j][i] = matrix[i][j];
-        }
-    }
-    console.log(grid);
-}
-matrixTransp(matrix1);
+//task 2
+//write method myMap for the same functiomality as standart method map
+//function myMap(array, callback-function)
+//myMap will apply your method myForEach
